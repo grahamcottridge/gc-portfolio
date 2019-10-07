4e1635dc5c6055dfa6109bcbab4649457444a84f
Graham Cottridge <codinggc@gmail.com>
2019-10-07 23:49:28 +0100

    Update styles and project item component

--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -2,22 +2,39 @@ import React from "react";
 
 import ProjectItem from "../project-item/project-item.component";
 
-import imageOne from "../../assets/music-event.png";
+import imageOne from "../../assets/clothing-store.jpg";
+import imageTwo from "../../assets/clothing-store.jpg";
+import imageThree from "../../assets/rsvp.jpg";
+import imageFour from "../../assets/music-event.jpg";
 
 import "./projects.styles.scss";
 
 const projectItems = [
   {
     id: 1,
-    title: "Project 1",
+    title: "Clothing Store",
     image: imageOne,
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
   },
   {
     id: 2,
-    title: "Project 2",
-    image: imageOne,
+    title: "Face Recognition",
+    image: imageTwo,
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: 3,
+    title: "Event Organiser",
+    image: imageThree,
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: 4,
+    title: "Music Event",
+    image: imageFour,
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
   }
