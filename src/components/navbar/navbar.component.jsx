23ef50dcb8805e35fc6f87704ae18b5b93595c81
Graham Cottridge <codinggc@gmail.com>
2019-10-12 23:33:17 +0100

    Install bootstrap and update menu component

--- src/components/navbar/navbar.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import "./navbar.styles.scss";
-
-const Navbar = () => <div></div>;
-
-export default Navbar;
--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -3,45 +3,22 @@ import React from "react";
 import Card from "../components/card/card.component";
 import About from "../components/about/about.component";
 import Projects from "../components/projects/projects.component";
-
-import Menu from "../components/_menu/_menu.component";
-import Nav from "../components/_nav/_nav.component";
-import Intro from "../components/_intro/_intro_component";
+import Menu from "../components/menu/menu.component";
 
 import "./homepage.styles.scss";
 
-class HomePage extends React.Component {
-  state = {
-    menuState: false
-  };
-
-  toggleMenu = () => {
-    this.setState(state => ({
-      menuState: !state.menuState
-        ? "active"
-        : state.menuState === "deactive"
-        ? "active"
-        : "deactive"
-    }));
-  };
-
-  render() {
-    return (
-      <div>
-        <Menu toggleMenu={this.toggleMenu} showMenu={this.state.menuState} />
-        <Nav toggleMenu={this.toggleMenu} showMenu={this.state.menuState} />
-        <Intro />
-        <div className="homepage">
-          <Card title="About me" id="about">
-            <About />
-          </Card>
-          <Card title="Projects" id="projects">
-            <Projects />
-          </Card>
-        </div>
-      </div>
-    );
-  }
-}
+const HomePage = () => (
+  <div>
+    <div className="homepage">
+      <Menu />
+      <Card title="About me" id="about">
+        <About />
+      </Card>
+      <Card title="Projects" id="projects">
+        <Projects />
+      </Card>
+    </div>
+  </div>
+);
 
 export default HomePage;
