c5deb4d2c701a3d573ff7e259efc4ab89d0afc73
Graham Cottridge <codinggc@gmail.com>
2019-11-18 21:54:31 +0000

    Updates

---
+++ precache-manifest.b8edc0293564bb4a2a5cbbf66a3e74a7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c8f4c39da10f96e6cf7d5112a0675f",
+    "url": "/gc-portfolio/index.html"
+  },
+  {
+    "revision": "0f0dd8e3828f6b5f6fe6",
+    "url": "/gc-portfolio/static/css/main.7d853498.chunk.css"
+  },
+  {
+    "revision": "330805fa8d3096af4644",
+    "url": "/gc-portfolio/static/js/2.c1cfe13a.chunk.js"
+  },
+  {
+    "revision": "0f0dd8e3828f6b5f6fe6",
+    "url": "/gc-portfolio/static/js/main.ae4575fc.chunk.js"
+  },
+  {
+    "revision": "3683a615e5a125af8b07",
+    "url": "/gc-portfolio/static/js/runtime-main.5bd48d8e.js"
+  },
+  {
+    "revision": "625053725da8f3194e31ad2888ac02c0",
+    "url": "/gc-portfolio/static/media/clothing-store.62505372.jpg"
+  },
+  {
+    "revision": "86a1a81a0b61fdb1e0d1d34a7ef97350",
+    "url": "/gc-portfolio/static/media/face-detection.86a1a81a.jpg"
+  },
+  {
+    "revision": "f5c40f699b88aeb631d84e8206eaba98",
+    "url": "/gc-portfolio/static/media/image3.f5c40f69.jpg"
+  },
+  {
+    "revision": "e5e71e686c4d491266528555eeb6fe40",
+    "url": "/gc-portfolio/static/media/music-event.e5e71e68.jpg"
+  },
+  {
+    "revision": "bb8e2d8ac124f5c2126cf5eebf9fa44a",
+    "url": "/gc-portfolio/static/media/rsvp.bb8e2d8a.jpg"
+  }
+]);
