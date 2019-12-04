7a72c2e82176715fdadef3076602897bdaf90b64
Graham Cottridge <codinggc@gmail.com>
2019-12-04 23:52:56 +0000

    Update about me

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -7,12 +7,11 @@ const About = props => (
     <h2 className="title">About me</h2>
     <div className="about-me">
       <p>
-        A graduate of Le Wagon (Lisbon) — a highly rated, start-up focused,
-        9-week intensive coding boot camp learning the foundations of
-        programming and tools of web development concluding in building full
-        stack applications. The skills learnt have since been advanced and
-        broadened with further study, online courses and building personal
-        projects.
+        A graduate of Le Wagon — a highly rated, start-up focused, 9-week
+        intensive coding boot camp learning the foundations of programming and
+        tools of web development concluding in building full stack applications.
+        The skills learnt have since been advanced and broadened with further
+        study, online courses and building personal projects.
       </p>
       <p>
         As a continuous learner, now focused on building full stack applications
